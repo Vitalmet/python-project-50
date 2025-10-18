97d11730a7b72cf0946c9215aad445cf5f856810
Vitalmet <vuyko87@bk.ru>
2025-10-18 15:58:32 +0300

    project 50

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,29 @@
 import argparse
 
+
+def generate_diff(file_1, file_2,format_name='stylish'):
+    return f"Comparing {file_1} and {file_2} in {format_name} format"
+
+
 def main():
     parser = argparse.ArgumentParser(
-        description='Compare files and show differences'
+        description='Compares two configuration files and shows difference.',
+        prog='gendiff'
 )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', default='stylish')
+    parser.add_argument(
+        '-f', '--format',
+        default='stylish',
+        help='set format of output',
+        metavar='FORMAT'
+    )
     args = parser.parse_args()
 
-    print(f"Comparing {args.first_file} and {args.second_file} in {args.format} format")
+    diff = generate_diff(args.first_file, args.second_file, args.format)
+
+    print(diff)
+
+
 if __name__ == '__main__':
     main()
